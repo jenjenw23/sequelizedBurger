e7622e2d0a3ddc6074359c0a45999fd9134d530f
Woulas <jwoula01@presencehealth.net>
2018-03-18 21:38:02 -0500

    fixed index, lists correctly, needs button

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,9 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Deluxe Cheeseburger', false);
+
+
+-- add new colums for sequilize
+ALTER TABLE burgers
+ADD COLUMN createdAt DATETIME NOT NULL AFTER devoured;
+ADD COLUMN updatedAt DATETIME NOT NULL AFTER createdAt;
